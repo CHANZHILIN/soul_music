7bd71aa6d5b91f7303d0a0ef5ba2c8faf96d79f4
chanzhilin <1181785848@qq.com>
2019-06-20 17:03:25 +0800

    application

---
+++ src/main/java/com/soul_music/application/MusicApplication.kt
@@ -0,0 +1,22 @@
+package com.soul_music.application
+
+import android.app.Application
+import com.kotlin_baselib.application.BaseApplication
+import com.kotlin_baselib.application.IComponentApplication
+
+/**
+ *  Created by CHEN on 2019/6/20
+ *  Email:1181785848@qq.com
+ *  Package:com.soul_music.application
+ *  Introduce:
+ **/
+class MusicApplication :IComponentApplication {
+
+    lateinit var instance: BaseApplication
+    override val application: Application
+        get() = instance
+
+    override fun onCreate(application: BaseApplication) {
+        instance = application
+    }
+}
