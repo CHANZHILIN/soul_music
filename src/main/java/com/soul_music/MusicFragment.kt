1080a79a5eab1ecd1fe4889038409f2c4d78ab59
chanzhilin <1181785848@qq.com>
2019-12-11 17:45:03 +0800

    图片首页，音频首页，视频首页，sharedElement

--- src/main/java/com/soul_music/MusicFragment.kt
+++ src/main/java/com/soul_music/MusicFragment.kt
@@ -1,9 +1,16 @@
 package com.soul_music
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotlin_baselib.api.Constants
 import com.kotlin_baselib.base.BaseViewModelFragment
 import com.kotlin_baselib.base.EmptyViewModel
+import com.kotlin_baselib.recyclerview.setSingleUp
+import com.kotlin_baselib.utils.SdCardUtil
+import com.soul_music.entity.MusicEntity
+import com.soul_picture.decoration.LinearLayoutDividerItemDecoration
 import kotlinx.android.synthetic.main.fragment_music.*
+import kotlinx.android.synthetic.main.layout_item_music.view.*
 
 
 private const val ARG_PARAM = "param1"
@@ -15,6 +22,9 @@ private const val ARG_PARAM = "param1"
  *  Introduce: 音乐Fragment
  **/
 class MusicFragment : BaseViewModelFragment<EmptyViewModel>() {
+
+
+    override fun getResId(): Int = R.layout.fragment_music
 
     private var param1: String? = null
 
@@ -31,26 +41,60 @@ class MusicFragment : BaseViewModelFragment<EmptyViewModel>() {
     companion object {
         @JvmStatic
         fun newInstance(param1: String) =
-                MusicFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM, param1)
-                    }
+            MusicFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM, param1)
                 }
+            }
     }
 
-    override fun getResId(): Int {
-        return R.layout.fragment_music
-    }
 
     override fun initData() {
-        fragment_text.setText(param1)
+
+        val musicSrc = SdCardUtil.DEFAULT_RECORD_PATH;
+        val fileData = SdCardUtil.getFilesAllName(musicSrc)
+
+        val pictureData = ArrayList<MusicEntity>()
+        for (fileDatum in fileData) {
+            pictureData.add(MusicEntity(fileDatum))
+        }
+
+        fragment_music_recyclerview.setSingleUp(
+            pictureData,
+            R.layout.layout_item_music,
+            LinearLayoutManager(mContext),
+            { holder, item ->
+                holder.itemView.item_music_tv_audio_name.text = item.path
+            },
+            {
+                /*     SnackbarUtil.ShortSnackbar(
+                         fragment_picture_recyclerview,
+                         "点击${it.picturePath}！",
+                         SnackbarUtil.CONFIRM
+                     )
+                         .show()*/
+            }
+        )
+
+        fragment_music_recyclerview.addItemDecoration(
+            LinearLayoutDividerItemDecoration(
+                mContext,
+                Constants.ITEM_SPACE
+            )
+        )
+        /*    fragment_picture_recyclerview.addItemDecoration(
+                PictureFragmentStaggeredDividerItemDecoration(
+                    mContext,
+                    ITEM_SPACE
+                )
+            )*/
     }
 
     override fun initListener() {
-      /*  float_action_button.setOnClickListener {
-            ARouter.getInstance().build(Constants.NRECORD_AUDIO_ACTIVITY_PATH).navigation()
+        /*  float_action_button.setOnClickListener {
+              ARouter.getInstance().build(Constants.NRECORD_AUDIO_ACTIVITY_PATH).navigation()
 
-        }*/
+          }*/
     }
 
 
---
+++ src/main/java/com/soul_music/entity/MusicEntity.kt
@@ -0,0 +1,9 @@
+package com.soul_music.entity
+
+/**
+ *  Created by CHEN on 2019/12/11
+ *  Email:1181785848@qq.com
+ *  Introduce: 音频的实体类
+ **/
+data class MusicEntity(val path:String) {
+}
